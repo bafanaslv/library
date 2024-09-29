e633d9e98d89ba3163cc75eba71ace42d7a32e24
Вячеслав Лукин <bafana.slv@gmail.com>
2024-09-29 09:06:19 +0300

    Create validators and generics.

--- library/validators.py
+++ library/validators.py
@@ -1,13 +1,13 @@
 from rest_framework.serializers import ValidationError
 
-from library.models import Books
+from library.models import Lending
 
 
 class LibraryValidators:
     def __call__(self, value):
         val = dict(value)  # конвертируем QuerySet в словарь
-        id_book = val["book"].pk
-        book_object = Books.objects.get(pk=id_book)
+        book_id = val["book"].pk
+        book_object = Lending.objects.get(id_book=book_id)
         print(book_object.pk)
         if book_object:
             raise ValidationError(
