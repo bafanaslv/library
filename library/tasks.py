51a35024f6250c0d5cfe5a5a4fa81b6c79dfa2c1
Вячеслав Лукин <bafana.slv@gmail.com>
2024-10-09 06:47:48 +0300

    Create Celery.

--- library/tasks.py
+++ library/tasks.py
@@ -5,20 +5,20 @@ from django.core.mail import send_mail
 from django.utils import timezone
 
 from config.settings import EMAIL_HOST_USER
-from library.models import Books, Lending
+from library.models import Lending
 from users.models import Users
 
 
 @shared_task
 def send_mail_return_books():
-    """Функция отправки уведомлений при обновлении курса"""
-    # Полученаем подписчиков
-    books = Lending.objects.filter(date_return=None)
-    if books:
-        for book in books:
-            to_email = book.user.email
+    """Функция отправки уведомлений читателям о небходимости возвата книг."""
+    # Получаем список книг не возвращненых читателями.
+    books_for_return = Lending.objects.filter(date_return=None)
+    if books_for_return:
+        for book_for_return in books_for_return:
+            to_email = book_for_return.user.email
             subject = "Возврат книги"
-            message = "Вы должны вернуть книгу"
+            message = f"Вы должны вернуть книгу {book_for_return.book.name}"
             send_mail(
                 subject=subject,
                 message=message,
@@ -27,15 +27,15 @@ def send_mail_return_books():
                 fail_silently=True,
             )
 
-
-@shared_task
-def check_login():
-    """Проверяет и деактивирует пользователей, которые не заходили в систему в течении 30 дней."""
-    users = Users.objects.filter(
-        last_login__lte=timezone.now() - timedelta(days=30), is_active=True
-    )
-    for user in users:
-        # обходим суперпользователя
-        if not user.is_superuser:
-            user.is_active = False
-            user.save()
+#
+# @shared_task
+# def check_login():
+#     """Проверяет и деактивирует пользователей, которые не заходили в систему в течении 30 дней."""
+#     users = Users.objects.filter(
+#         last_login__lte=timezone.now() - timedelta(days=30), is_active=True
+#     )
+#     for user in users:
+#         # обходим суперпользователя
+#         if not user.is_superuser:
+#             user.is_active = False
+#             user.save()
